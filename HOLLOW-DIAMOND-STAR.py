73aa59c98c038337b300f387955bb20f65f28aea
Gowtham <79574853+KarshGowthamofficial@users.noreply.github.com>
2022-09-11 21:53:11 +0530

    Update HOLLOW-DIAMOND-STAR.py

--- HOLLOW-DIAMOND-STAR.py
+++ HOLLOW-DIAMOND-STAR.py
@@ -17,3 +17,17 @@ for i in reversed(range(n-1)):
       *   * 
         *
     '''
+    
+
+for i in range(n):
+    print(" "*(n-i-1)+"*",end="")
+    if i>=1:
+        print(" "*(2*i-1)+"*",end="")
+    print()
+
+for i in reversed(range(n-1)):
+    print(" "*(n-i-1)+"*",end="")
+    if i>=1:
+        print(" "*(2*i-1)+"*",end="")
+    print()
+
